9d3138683cb3c6eb2ec0cf412659dc1976d76092
Kenneth Iwuchukwu <52069413+keonramses@users.noreply.github.com>
2022-06-25 15:39:42 -0700

    Update login.php
    - called function to destroy session before initializing a new one

--- page/login.php
+++ page/login.php
@@ -56,6 +56,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                     if(mysqli_stmt_fetch($stmt)){
                         if(password_verify($password, $hashed_password)){
                             // Password is correct, so start a new session
+                            session_destroy();
                             session_start();
                             
                             // Store data in session variables
